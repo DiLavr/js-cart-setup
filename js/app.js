4bc269ece69d34ae9d66d2c0268bf1f8bb0efc0e
DiLavr <42397553+DiLavr@users.noreply.github.com>
2019-11-12 09:04:50 -0500

    starting app.js

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// show the card
+
+(function () {
+
+    const cartInfo = document.getElementById('cart-info');
+    const cart = document.getElementById('cart');
+
+    cartInfo.addEventListener('click', function () {
+
+        cart.class
+
+
+
+
+    })
+
+
+
+
+}
+
+
+
+
+
+)
